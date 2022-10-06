543a50f0e112aa88e84e610c93e7a33d373b47c9
hiverkiya <hiverkiya@gmail.com>
2022-10-06 22:44:52 +0530

    initialBuild

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import {SessionProvider} from "next-auth/react"
+import {RecoilRoot} from "recoil"
+// adding session provider to persist the login state
+function MyApp({ Component, pageProps :{ session,...pageProps} }) {
+  return <SessionProvider session={session}>
+    <RecoilRoot>
+    <Component {...pageProps} />
+    </RecoilRoot>
+  </SessionProvider>
+}
+
+export default MyApp

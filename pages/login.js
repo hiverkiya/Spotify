24a0b34bf8488c8eb855080401645a63155615cb
hiverkiya <hiverkiya@gmail.com>
2022-10-09 02:03:13 +0530

    minorFix

--- pages/login.js
+++ pages/login.js
@@ -4,13 +4,15 @@ import React from "react";
 function Login({ providers }) {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt=""/>
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
       {Object.values(providers).map((provider) => (
-        <div key ={provider.name}>
+        <div key={provider.name}>
           <button
-          onClick={()=> signIn(provider.id,{callbackUrl:"/"})}
-          className="bg-[#18D860] text-white  p-5  rounded-full "
-          >Login with {provider.name}</button>
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            className="bg-[#18D860] text-white  p-5  rounded-full hover:opacity-75 "
+          >
+            Login with {provider.name}
+          </button>
         </div>
       ))}
     </div>

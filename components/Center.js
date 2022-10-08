1dffafe888c980bc286347270e0ea3e7be5b5302
hiverkiya <hiverkiya@gmail.com>
2022-10-09 00:04:41 +0530

    Reformatting and fixing minor issues

--- components/Center.js
+++ components/Center.js
@@ -1,4 +1,4 @@
-import { ChevronDownIcon } from "@heroicons/react/outline";
+import { LogoutIcon } from "@heroicons/react/outline";
 import React, { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -10,12 +10,12 @@ const colors = [
   "from-indigo-500",
   "from-blue-500",
   "from-green-500",
-  "from-red-500",
   "from-yellow-500",
   "from-pink-500",
   "from-purple-500",
+  "from-teal-500",
+  "from-rose-500",
 ];
-
 function Center() {
   const { data: session } = useSession();
   const spotifyApi = useSpotify();
@@ -34,36 +34,41 @@ function Center() {
       })
       .catch((err) => console.log("This error happened: ", err));
   }, [spotifyApi, playlistId]);
-  console.log(playlist)
+  console.log(playlist);
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center text-white bg-black space-x-3 opacity-90 p-1 pr-2 hover:opacity-80 cursor-pointer rounded-full "
-        onClick={signOut}>
+        <div
+          className="flex items-center text-white bg-black space-x-3 opacity-100 p-1 pr-2 hover:opacity-80 cursor-pointer rounded-full "
+          onClick={signOut}
+        >
           <img
-            className="rounded-full w-10 h-10"
+            className="rounded-full w-12 h-12"
             src={session?.user.image}
             alt=""
           />
           <h2>{session?.user.name}</h2>
-          <ChevronDownIcon className="h-5 w-5" />
+          <LogoutIcon className="h-6 w-6" />
         </div>
       </header>
       <section
         className={` flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
-        <img className="h-44 w-44 shadow-2xl"
-        src={playlist?.images?.[0]?.url}
-        alt=""
+        <img
+          className="h-44 w-44 shadow-2xl"
+          src={playlist?.images?.[0]?.url}
+          alt=""
         />
 
-       <div>
-         <p>PLAYLIST</p> {/* First value is always mobile value */}
-         <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1> 
-       </div>
+        <div>
+          <p>PLAYLIST</p> {/* First value is always mobile value */}
+          <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+            {playlist?.name}
+          </h1>
+        </div>
       </section>
       <div>
-        <Songs/>
+        <Songs />
       </div>
     </div>
   );
